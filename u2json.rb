41d8327e6d153e8c1ff1786296dfbdc2f39c6a33
Victor Roemer <viroemer@cisco.com>
2015-05-24 16:02:08 -0400

    jansson is a dependency.

--- u2json.rb
+++ u2json.rb
@@ -6,6 +6,7 @@ class U2json < Formula
   sha256 "36eb6807e7dda88ae232a41f2c323c29f896c810c5cb0020e0415629e62cdba0"
 
   depends_on "libunified2"
+  depends_on "jansson"
 
   def install
     system "./configure", "--prefix=#{prefix}"
