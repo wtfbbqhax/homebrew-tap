972ec8cfda60d25a23824215f8c62f899cca7247
Victor Roemer <viroemer@cisco.com>
2015-05-24 15:09:11 -0400

    fix depends

--- u2json.rb
+++ u2json.rb
@@ -5,6 +5,8 @@ class U2json < Formula
   url "https://github.com/wtfbbqhax/u2json/releases/download/v1.0/u2json-1.tar.gz"
   sha256 "36eb6807e7dda88ae232a41f2c323c29f896c810c5cb0020e0415629e62cdba0"
 
+  depends_on "libunified2"
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
