efbd91a025e10eb773a6a9187e364d7d32b80db2
Victor Roemer <viroemer@cisco.com>
2015-05-24 15:06:38 -0400

    u2json

---
+++ u2json.rb
@@ -0,0 +1,12 @@
+require "formula"
+
+class U2json < Formula
+  homepage "https://github.com/wtfbbqhax/u2json"
+  url "https://github.com/wtfbbqhax/u2json/releases/download/v1.0/u2json-1.tar.gz"
+  sha256 "36eb6807e7dda88ae232a41f2c323c29f896c810c5cb0020e0415629e62cdba0"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
